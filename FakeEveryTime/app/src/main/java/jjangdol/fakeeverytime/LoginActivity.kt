e7d725dd07fa29964b8b8ed9582e356eb33a2369
vkqxkr <36595743+vkqxkr@users.noreply.github.com>
2020-03-23 15:57:21 +0900

    개발중

--- FakeEveryTime/app/src/main/java/jjangdol/fakeeverytime/LoginActivity.kt
+++ FakeEveryTime/app/src/main/java/jjangdol/fakeeverytime/LoginActivity.kt
@@ -1,8 +1,12 @@
 package jjangdol.fakeeverytime
 
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_login.*
 
 class  LoginActivity : AppCompatActivity() {
@@ -10,6 +14,18 @@ class  LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //상태바 투명 및 상태바 아이콘을 어둡게 설정
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = 0x00000000  // transparent
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val flags = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+            window.addFlags(flags)
+        }
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 
         textView_search.setOnClickListener {
             val goSearchInfoActivity = Intent(this, SearchInfoActivity::class.java)
