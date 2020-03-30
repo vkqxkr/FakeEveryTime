6fd34f7283152b0937a100722888c898c72c5322
vkqxkr <36595743+vkqxkr@users.noreply.github.com>
2020-03-30 16:21:42 +0900

    수정

--- FakeEveryTime/app/src/main/java/jjangdol/fakeeverytime/LoginActivity.kt
+++ FakeEveryTime/app/src/main/java/jjangdol/fakeeverytime/LoginActivity.kt
@@ -1,31 +1,22 @@
 package jjangdol.fakeeverytime
 
 import android.content.Intent
-import android.graphics.Color
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
+
 
 class  LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
 
-        //상태바 투명 및 상태바 아이콘을 어둡게 설정
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.statusBarColor = 0x00000000  // transparent
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            val flags = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-            window.addFlags(flags)
-        }
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        // 상태바 회색 아이콘 색상을 설정
+        val view = window.decorView
+        view.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+        setContentView(R.layout.activity_login)
 
         textView_search.setOnClickListener {
             val goSearchInfoActivity = Intent(this, SearchInfoActivity::class.java)
--- FakeEveryTime/app/src/main/java/jjangdol/fakeeverytime/SearchInfoActivity.kt
+++ FakeEveryTime/app/src/main/java/jjangdol/fakeeverytime/SearchInfoActivity.kt
@@ -2,11 +2,16 @@ package jjangdol.fakeeverytime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SearchInfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 상태바 회색 아이콘 색상을 설정
+        val view = window.decorView
+        view.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         setContentView(R.layout.activity_search_info)
     }
 }
